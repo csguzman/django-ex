bb4a3e1d4fa1e52071834059807fa7c407674684
carlos <carlos.sguzman@gmail.com>
2017-09-13 13:38:38 +0200

    configured URL Debug to True

--- pelis/urls.py
+++ pelis/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', index),
-    url(r'^health$', health),
+    # url(r'^$', index),
+    # url(r'^health$', health),
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^', include('alarms.urls')),
+    url(r'^', include('alarms.urls')),
 ]
