c3d780907f01f1ecb5db944ecfeae74853e5ffe2
carlos <carlos.sguzman@gmail.com>
2017-09-13 18:39:57 +0200

    moved static files

--- pelis/settings.py
+++ pelis/settings.py
@@ -166,12 +166,18 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # CUSTOM CONFIG
 
