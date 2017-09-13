3b68e331a2730b510a97b0a8725c7dcb195c449a
carlos <carlos.sguzman@gmail.com>
2017-09-13 12:53:10 +0200

    back to previous config

--- pelis/settings.py
+++ pelis/settings.py
@@ -139,7 +139,7 @@ WSGI_APPLICATION = 'wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(DATA_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
