d62d266486246ac23a713733d2c48d3704568697
carlos.guzman <carlos@icemobile.com>
2018-06-01 13:43:06 +0200

    used whitenoise to serve static files

--- pelis/settings.py
+++ pelis/settings.py
@@ -188,6 +188,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
@@ -195,7 +196,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+os.makedirs(STATIC_TMP, exist_ok=True)
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = [
 #     os.path.join(PROJECT_ROOT, 'static'),
