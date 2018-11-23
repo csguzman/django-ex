bf8cfc1c829183372dc3ae5f807021573dac7202
carlos <carlos.sguzman@gmail.com>
2018-11-23 17:31:51 +0100

    Improved stand-alone send push and post tweet functions

--- alarms/management/commands/post_tweet.py
+++ alarms/management/commands/post_tweet.py
@@ -1,10 +1,10 @@
-from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
-from urllib.request import Request, urlopen, URLError, HTTPError, urlretrieve
-from alarms.objects import Channel, Program
-from datetime import datetime, timezone
 import json
+from urllib.request import urlopen, HTTPError
+import requests
+
 import tweepy
+from django.conf import settings
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
@@ -13,294 +13,38 @@ class Command(BaseCommand):
     default_channels = ['La 1', 'La 2', 'Antena 3', 'Cuatro', 'Telecinco', 'laSexta', 'Teledeporte', 'Clan TVE', 'Canal 24 horas', 'Nova', 'Neox', 'Mega', 'FDF Telecinco', 'Energy', 'Divinity', 'Boing', 'Disney Channel', 'Intereconomía TV', 'Paramount Channel', '13tv', 'Discovery MAX', 'Atreseries', 'Be Mad']
     image_url = 'https://programacion-tv.elpais.com/%s'
 
+    dynamic_link_json = "{\"dynamicLinkInfo\": {\"dynamicLinkDomain\": \"e2ays.app.goo.gl\",\"link\": \"https://pelisdeldia.com/?programId=%s\",\"androidInfo\": {\"androidPackageName\": \"com.csalguero.onlymovies\",\"androidFallbackLink\": \"https://play.google.com/store/apps/details?id=com.csalguero.onlymovies\"},\"iosInfo\": {\"iosBundleId\": \"com.csalguero.onlymovies\",\"iosFallbackLink\": \"https://itunes.apple.com/es/app/apple-store/id1235072016\",\"iosAppStoreId\": \"1235072016\"}},\"suffix\": {\"option\": \"SHORT\"}}"
+
+    def add_arguments(self, parser):
+        parser.add_argument('parameter', nargs='+', type=str)
+
+# programId
+# title
+# time
+# channel
+# imageUrl
     def handle(self, *args, **options):
         self.stdout.write('enter command')
+        parameter_str = options['parameter'][0]
+        self.stdout.write('parameters:  %s' % parameter_str)
+        parameter_json = json.loads(parameter_str)
+        title = parameter_json['TITULO']
+        id_programa = parameter_json['EVENTO']
+        channelName = parameter_json['channelName']
+        start_time = parameter_json['HORA_INICIO']
+        if 'tweetImage' in parameter_json and len(parameter_json["tweetImage"]) != 0:
+            tweet_image = parameter_json['tweetImage']
 
-        # Get the channel list from server
-        self.channel_list_json = Command.get_channel_list_as_json()
-        # created = channel_list_json.get("created", None)
-        # data = self.get_data(channel_list_json)
-        self.channels = self.get_channel_list(self.channel_list_json)
+        parameter_json["URL"] += '&id=%s' % id_programa
 
-        all_movies = self.get_grid_data()
+        print('generating link')
+        bodyDict = json.loads(self.dynamic_link_json)
+        link = bodyDict["dynamicLinkInfo"]["link"] % id_programa
+        bodyDict["dynamicLinkInfo"]["link"] = link
+        link = self.generate_dynamic_link(json.dumps(bodyDict))
 
-        all_programs = self.get_movies_with_rating(all_movies)
-
-        # # for each channel, get all the films to be played
-        # for channel in channels:
-        #     programs = self.get_channel_programs(channel, data, True)
-        #     all_programs.extend(programs)
-
-        all_programs.sort(key=lambda r: (int(r.artistic) + int(r.commercial)), reverse=True)
-        # self.stdout.write("******** ordering ********")
-        gcm_message = "";
-
-        json_string = json.dumps(all_programs[0], default=lambda o: o.__dict__)
-        # json_string = json.dumps([program.__dict__ for program in all_programs])
-        # jsonString = json.dumps(all_programs.__dict__)
-        print(json_string)
-        # for program in all_programs:
-        # jsonString = json.dumps(program.__dict__)
-        # self.stdout.write("%s -> %s -> %s" % (program.programTitle, program.startDateTime, program.channel))
-        # gcm_message += str("%s -> %s -> %s\n" % (program.program_title, program.start_datetime, program.channel))
-
-        fmt = '%Y-%m-%d %H:%M:%S'
-        ini_datetime = datetime.strptime(all_programs[0].iniDate, fmt)
-
-        message_title = '%s, a las %s:%s' % (all_programs[0].get_visible_title(),
-                                             ('%02d' % ini_datetime.hour),
-                                             ('%02d' % ini_datetime.minute))
-
-        channelName = self.channel_list_json[all_programs[0].idCanal]["nombre"]
-
-        program_image = self.image_url % all_programs[0].image
-        tweet = '%s, a las %s:%s, en %s. %s.' % (all_programs[0].get_visible_title(),
-                                                 ('%02d' % ini_datetime.hour),
-                                                 ('%02d' % ini_datetime.minute),
-                                                 channelName,
-                                                 'https://pelisdeldiaco78.app.link/?programId=%s' %
-                                                 all_programs[0].id_programa)
-
-        self.post_tweet(program_image, tweet)
-        self.stdout.write(json_string)
-        self.stdout.write('done')
-
-    @staticmethod
-    def get_channel_list_as_json():
-        """
-        # {
-        #     "716": {
-        #         "id": "716",
-        #         "name": "Paramount Channel",
-        #         "lang": "SPA",
-        #         "order": "21"
-        #     },
-        #     "841": {
-        #         "id": "841",
-        #         "name": "Mega",
-        #         "lang": "SPA",
-        #         "order": "12"
-        #     },
-        #     "created": "2015-11-12",
-        #     "data1029935091": "d6c7ad3fd92f295c7f042a31e15f8438,2fb866e798afe8b0ea7a5fd87a41c85c,55b8c67bf24c5ce95627fd7da8f9318d,71d043c367eeeefc623e902e80ebb75e,5571cb887ee725aeac1520b0c5b0b17f,182ba4d2978bdc4e48f6039bf12db38c,b4a7efcb96a4d56a4c82b4dc1c84662b"
-        # }
-        :return:
-        """
-        # Get the dataset
-        req = Request('https://programacion-tv.elpais.com/data/canales.json',
-                      headers={'User-Agent': 'Mozilla/5.0'})
-        # url = 'http://www.mediadata.tv/exports/mediadata/data/channel/list.json'
-        response = urlopen(req)
-
-        # Convert bytes to string type and string type to dict
-        string = response.read().decode('utf-8')
-        json_obj = json.loads(string)
-        return json_obj
-
-    def get_data(self, channel_list_json):
-        data = next(v for k, v in channel_list_json.items() if k.startswith('data'))
-        return data.split(',')[0]
-
-    def get_channel_list(self, channel_list_json):
-        channels = []
-        for key, value in channel_list_json.items():
-            if isinstance(value, dict):
-                channel_name = value.get("nombre", None)
-                channel_id = value.get("id", None)
-                channel = Channel(channel_id, channel_name)
-                    # self.stdout.write(channel.name)
-                if any(channel_name in s for s in self.default_channels):
-                    channels.append(channel)
-        return channels
-
-    def get_grid_data(self):
-        utc_dt = datetime.now(timezone.utc)
-        today_date = utc_dt.strftime("%d%m%Y")
-
-        req = Request('https://programacion-tv.elpais.com/data/parrilla_%s.json'
-                      % today_date,
-                      headers={'User-Agent': 'Mozilla/5.0'})
-
-        response = urlopen(req)
-
-        # Convert bytes to string type and string type to dict
-        string = response.read().decode('utf-8')
-        programs_json = json.loads(string)
-
-        programs_array = []
-
-        for channelItem in programs_json:
-            # print(channelItem)
-            programs_array_json = channelItem.get('programas', None)
-
-            for program_item in programs_array_json:
-                description = program_item.get("description", None)
-                endDate = program_item.get("endDate", None)
-                idCanal = program_item.get("idCanal", None)
-                idSection = program_item.get("idSection", None)
-                id_programa = program_item.get("id_programa", None)
-                iniDate = program_item.get("iniDate", None)
-                name = program_item.get("name", None)
-                recommended = program_item.get("recommended", None)
-                title = program_item.get("title", None)
-                txtSection = program_item.get("txtSection", None)
-
-                program = Program(id_programa)
-
-                program.iniDate = str(iniDate)
-                program.endDate = str(endDate)
-                program.description = description
-                program.idCanal = idCanal
-                program.idSection = idSection
-                program.name = name
-                program.recommended = recommended
-                program.title = title
-                program.txtSection = txtSection
-
-                fmt = '%Y-%m-%d %H:%M:%S'
-                ini_datetime = datetime.strptime(program.iniDate, fmt)
-                channel_name = self.channel_list_json[idCanal]["nombre"]
-
-                if any(channel_name in s.name for s in self.channels):
-                    if txtSection == 'Películas' and ini_datetime.hour > 14:
-                        programs_array.append(program)
-
-        return programs_array
-
-    def get_movies_with_rating(self, all_movies):
-
-        programs_array = []
-        for movie in all_movies:
-            group_id = int(int(movie.id_programa) / 10000)
-
-            if movie.get_visible_title() != "":
-
-                req = Request('https://programacion-tv.elpais.com/data/programas/%s.json'
-                              % group_id,
-                              headers={'User-Agent': 'Mozilla/5.0'})
-
-                response = urlopen(req)
-
-                # Convert bytes to string type and string type to dict
-                string = response.read().decode('utf-8')
-                programs_json = json.loads(string)
-
-                utc_dt = datetime.now(timezone.utc)
-                fmt = '%Y-%m-%d %H:%M:%S'
-
-                for program_json in programs_json:
-                    if movie.id_programa == program_json.get('id_programa', None):
-                        # print(program_json)
-
-                        artistic = int(program_json.get('artistic', None))
-                        commercial = int(program_json.get('commercial', None))
-                        image = program_json.get('image', None)
-
-                        movie.artistic = artistic
-                        movie.commercial = commercial
-                        movie.image = image
-
-                        ini_datetime = datetime.strptime(movie.iniDate, fmt)
-
-                        if utc_dt.day == ini_datetime.day:
-                            programs_array.append(movie)
-                        break
-                    else:
-                        continue
-
-        return programs_array
-
-    def get_channel_programs(self, channel, data, only_movies):
-        """
-            # {
-        #     "180895_27249973": {
-        #         "id": "180895_27249973",
-        #         "channel_id": "841",
-        #         "program_id": "180895",
-        #         "event_id": "27249973",
-        #         "lang": "SPA",
-        #         "date_start": "2015-11-12",
-        #         "date_end": "2015-11-12",
-        #         "start_time": "04:50",
-        #         "end_time": "06:55",
-        #         "program_title": "El cÃ­rculo secreto",
-        #         "section": "Series",
-        #         "description": "Cassie se sorprende cuando un amigo del pasado aparece en la puerta de su casa",
-        #         "height": "284"
-        #     }
-        # }
-
-
-
-        :param self:
-        :param channel:
-        :param data:
-        :param only_movies:
-        :return:
-        """
-        req = Request('http://www.mediadata.tv/exports/mediadata/data/event/list/channel/%s/date/%s.json'
-                      % (channel.channel_id, data),
-                      headers={'User-Agent': 'Mozilla/5.0'})
-        # url = 'http://www.mediadata.tv/exports/mediadata/data/event/list/channel/%s/date/%s.json' % (channel.channel_id, data)
-        response = urlopen(req)
-
-        # Convert bytes to string type and string type to dict
-        string = response.read().decode('utf-8')
-        programs_json = json.loads(string)
-
-        programs = []
-        for key, value in programs_json.items():
-            if isinstance(value, dict):
-                program_title = value.get("program_title", None)
-                program_id = value.get("program_id", None)
-                section = value.get("section", None)
-                description = value.get("description", None)
-                date_start = value.get("date_start", None)
-                date_end = value.get("date_end", None)
-                start_time = value.get("start_time", None)
-                end_time = value.get("end_time", None)
-                lang = value.get("lang", None)
-
-                program = Program(program_id, program_title, section, channel.channel_id, lang)
-
-                fmt = '%Y-%m-%d %H:%M'
-                program.date_start = str(datetime.strptime('%s %s' % (date_start, start_time), fmt))
-                program.description = description
-                program.date_start = str(date_start)
-                program.date_end = str(date_end)
-                program.start_time = str(start_time)
-                program.end_time = str(end_time)
-
-                if only_movies:
-                    if section == 'Peliculas':
-                        # self.stdout.write('%s:  %s' % (program.programTitle, channel.name))
-                        self.get_program_details(program)
-                        if (program.artistic is not None) and (program.commercial is not None):
-                            if (int(program.artistic) >= 4) or (int(program.commercial) >= 4):
-                                programs.append(program)
-                                # self.stdout.write('%s:  %s -> %s' % (program.program_title, channel.name, program.image))
-                else:
-                    programs.append(program)
-                    self.stdout.write(program.program_title)
-
-        return programs
-
-    def get_program_details(self, program):
-
-        req = Request('http://www.mediadata.tv/exports/mediadata/data/program/%s/%s.json'
-                      % (program.lang, program.program_id),
-                      headers={'User-Agent': 'Mozilla/5.0'})
-        # url = 'http://www.mediadata.tv/exports/mediadata/data/program/%s/%s.json' % (program.lang, program.programId)
-        response = urlopen(req)
-
-        # Convert bytes to string type and string type to dict
-        string = response.read().decode('utf-8')
-        program_json = json.loads(string)
-        program.image = program_json.get("image", None)
-        program.artistic = program_json.get("artistic", None)
-        program.commercial = program_json.get("commercial", None)
-
-        # self.stdout.write(('val-> artistic: %s, commercial: %s' % (program.artistic, program.commercial))
+        tweet = '%s, a las %s, en %s. %s.' % (title, start_time, channelName, link)
+        self.post_tweet(tweet_image, tweet)
 
     def get_twitter_api(self, cfg):
         auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
@@ -330,3 +74,19 @@ class Command(BaseCommand):
             print('image downloading failed, default tweet')
             status = api.update_status(status=message)
             print(status)
+
+    def generate_dynamic_link(self, body):
+        url = "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=AIzaSyBslo51LLKYhjKCS9rlrrCoi7HvE3HbhJw"
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        data = body
+
+        r = requests.post(url, headers=headers, data=data)
+        print(r.status_code)
+        print(r.json())
+        if r.status_code == 200:
+            result = r.json()
+            return result['shortLink']
+        else:
+            return ""
